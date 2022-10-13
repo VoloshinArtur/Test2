f14b74e60ce7735267e95ca0e0e937c21f35ead0
VoloshinArtur <115408461+VoloshinArtur@users.noreply.github.com>
2022-10-13 21:09:43 +0300

    Add files via upload
    определение корней

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int a, b, c, D;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	D = b * b - 4 * a * c;
+	if (D>0)
+	{
+		cout << "has 2 roots";
+	}
+	else if (D<0)
+	{
+		cout << "no root";
+	}
+	else
+	{
+		cout << "root = 0";
+	}
+}
